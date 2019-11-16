96e8e72b103112fd7e0a6b687dd8cb4c2719b5b5
meebs <alex@takayamasan.com>
2019-11-16 15:17:39 -0500

    revamped main

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
  * The main function.
  */
 function main() {
-  const credentialsFilePath = "../credentials.txt";
+  const credentialsFilePath = "../credentials.json";
 
   // read in the file as a JSONObject, with props accountSid and authToken.
   const credentialsFile = fs.readFileSync(credentialsFilePath, "utf-8");
@@ -12,21 +12,24 @@ function main() {
   
   const accountSid = credentialsObj.accountSid;
   const authToken = credentialsObj.authToken;
+  const number = credentialsObj.number;
 
   // launch the server.
-  launch(accountSid, authToken);
+  launch(accountSid, authToken, number);
 }
 
 /**
  * Launches the main server app.
  * @param {string} accountSid the account sid to use for twilio verification
  * @param {string} authToken the authentication token to use for twilio verification
+ * @param {string} number the phone number to send and receive from
  */
-async function launch(accountSid, authToken) {
+async function launch(accountSid, authToken, number) {
 
   // require twilio.
   const client = require('twilio')(accountSid, authToken);
 
-  
+  // our number! is a string.
+  const us = number;
 }
 
