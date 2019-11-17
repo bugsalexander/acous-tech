be8971e4bb53d56ac53e1561b10c59eca0d03170
meebs <alex@takayamasan.com>
2019-11-16 19:53:53 -0500

    commented out python fork

--- src/converter.js
+++ src/converter.js
@@ -10,14 +10,15 @@ export function convert(toConvert) {
   // the emoji converter can only convert lowercase strings.
   const lowercase = toConvert.toLowerCase();
 
+  return "this is test data, replace it later!";
   // create the child python process, and pipe it some input.
-  const spawn = require("child_process").spawn;
-  const pythonProcess = spawn('python',["./conversion.py", lowercase]);
+  // const spawn = require("child_process").spawn;
+  // const pythonProcess = spawn('python3',["./conversion.py", lowercase]);
 
-  // return a promise, resolved with the output of the child process.
-  return new Promise((resolve, reject) => {
-    pythonProcess.stdout.on('data', (data) => {
-      resolve(data);
-    });
-  });
+  // // return a promise, resolved with the output of the child process.
+  // return new Promise((resolve, reject) => {
+  //   pythonProcess.stdout.on('data', (data) => {
+  //     resolve(data);
+  //   });
+  // });
 }
